1c153b90b87eb9e00b7e4f61b109d600e402c7e8
田中緑(macbook) <evergreen.69rockn@gmail.com>
2020-05-07 22:42:30 +0900

    Updates

---
+++ precache-manifest.e504fb6d7cdb60df5cc7dd3ed43e2ea2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50ea7806896927b95ca45f78dd2c9721",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b602709e9b8a87aa3eec",
+    "url": "/static/css/main.ce08940a.chunk.css"
+  },
+  {
+    "revision": "06886a6af817b7369591",
+    "url": "/static/js/2.c2b703a0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c2b703a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b602709e9b8a87aa3eec",
+    "url": "/static/js/main.8640b5bb.chunk.js"
+  },
+  {
+    "revision": "2060b9a7e54a59ab7b07",
+    "url": "/static/js/runtime-main.f3a02c50.js"
+  },
+  {
+    "revision": "07de92b45ff95f1a205c82f12baf2790",
+    "url": "/static/media/main_bg_front.07de92b4.png"
+  },
+  {
+    "revision": "7488434dcee68b44563f0df85bd24c2a",
+    "url": "/static/media/profile_image.7488434d.jpg"
+  },
+  {
+    "revision": "a6f8e5c8243f1d63675c9368343bec94",
+    "url": "/static/media/thumbnail_01.a6f8e5c8.png"
+  },
+  {
+    "revision": "865dcf4ede775a8e93c69e21cde69cd8",
+    "url": "/static/media/thumbnail_02.865dcf4e.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mdrn.portfolio/precache-manifest.a62974799c1cdb0aa8b28027100d00aa.js"
+  "/precache-manifest.e504fb6d7cdb60df5cc7dd3ed43e2ea2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mdrn.portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
